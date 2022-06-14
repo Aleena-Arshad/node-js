268fc0acc6fd0eaebb62bdf077fd417d2ab9bf24
Aleena-Arshad <aleena.aarshad2000@gmail.com>
2022-06-14 12:31:30 +0500

    new

---
+++ index.js
@@ -0,0 +1,54 @@
+// var x= 20;
+// // if(x===20)
+// // {
+// //     console.log("matched")
+// // }
+
+// const app = require('./app')
+// console.log(app)
+// console.log(app.x)
+
+// const arr = [1,2,3,4,5,6,7,8,9];
+// let result = arr.filter((item)=>{
+//     // console.log(item)
+//     // return item === 3;
+//     return item > 3;
+// })
+// console.warn(result)
+
+// fs.writeFileSync("hello2.txt","like and subscribe")
+// console.log("->",__dirname);
+// const fs = require('fs');
+// fs.writeFileSync("code.txt","some code")
+
+// const { listenerCount } = require('events');
+// const { listenerCount } = require('events');
+
+// http handle request and response data that we take from server or send to the client
+// const http = require('http');
+// http.createServer((req, res) => {
+//     res.write("hello this is aleena arshad djjsdjsjdjs girl ....");
+//     res.end();
+// }).listen(5000);
+// or
+// const dataControl = (req,res)=>
+// {
+//     res.write("This is Aleena Arshad");
+//     res.end();
+// }
+// http.createServer(dataControl).listen(5000);
+
+
+// arrow functions
+
+// function test(a)
+// {
+//     return a* 10;
+// }
+// with arrow function
+// const test =(a)=> a*100
+
+// package.json: It keeps details of project (details related to code)
+// npm : node package manager
+
+ghbb
